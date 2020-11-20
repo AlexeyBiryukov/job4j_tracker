f139b6f167647648b5533d9006f7ed583cc2963b
Java <AlexeyBiryukov/job4j_elementary>
2020-11-20 20:42:05 +0300

    5. Tracker - хранилище [#303880]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,16 +4,8 @@ public class StartUI {
     public static void main(String[] args) {
         Tracker tracker = new Tracker();
         Item item1 = new Item();
-        Item item2 = new Item();
 
         tracker.add(item1);
-        tracker.add(item2);
-
-        item1.setName("item1");
-        item1.setId(1);
-
-        item2.setName("item2");
-        item2.setId(2);
 
         System.out.println(tracker.findById(1));
     }
